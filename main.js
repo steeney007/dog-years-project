eec857499cbd4ebafe534b4b4551bb6b4ddef052
steeney007 <116930468+steeney007@users.noreply.github.com>
2022-10-30 20:05:43 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+var myAge = 39;
+//variable named myage created to equal my age//
+let earlyYears = 2;
+//create variable called earlyYears that can change)//
+earlyYears *=  10.5;
+let laterYears = (myAge - 2);
+// create variable called laterYears that is myAge minus 2//
+laterYears *= 4;
+//use operator to change value of laterYears to laterYears multiplied by 4//
+console.log (earlyYears);
+console.log (laterYears);
+var myAgeInDogYears = (earlyYears + laterYears);
+//create variable called myAgeInDogYears that is sum of earlyYears and laterYears//
+var myName = 'christina'.toLowerCase();
+//create variable of my first name called myName in lower case using string//
+console.log (`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+//create console.log statement using string interpolation to state myname, age in human years, and coordinating age in dog years//
